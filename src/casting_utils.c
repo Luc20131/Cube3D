e3a8022bf5bde324a5a3bd0bca92bc5057710c7d
Lucas Richaud <82425994+Luc20131@users.noreply.github.com>
2024-11-19 16:15:34 +0100

    raycast normed (#13)

---
+++ src/casting_utils.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   casting_utils.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lrichaud <lrichaud@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/14 02:06:05 by lrichaud          #+#    #+#             */
+/*   Updated: 2024/11/19 14:47:28 by lrichaud         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cube3d.h"
+#define ROT_SPEED 2
+
+void	init_value_for_cast(t_ray *ray, t_mlx *vars, t_pos *origin)
+{
+	ray->dir_x = -0.5;
+	ray->dir_y = 0;
+	ray->plane_y = 0.66;
+    ray->plane_x = 0;
+//	 double old_dir_x = ray->dir_x;
+//	 ray->dir_x = ray->dir_x * cos(-ROT_SPEED) - ray->dir_y * sin(-ROT_SPEED);
+//	 ray->dir_y = old_dir_x * sin(-ROT_SPEED) + ray->dir_y * cos(-ROT_SPEED);
+//	 double old_plane_x = ray->plane_x;
+//	 ray->plane_x = ray->plane_x * cos(-ROT_SPEED) - ray->plane_y * sin(-ROT_SPEED);
+//	 ray->plane_y = old_plane_x * sin(-ROT_SPEED) + ray->plane_y * cos(-ROT_SPEED);
+	ray->hit = 0;
+	ray->map_pos = ray->initial_pos;
+	ray->camera_x = ((2 * origin->x) / (double) vars->img.w) - 1;
+	ray->ray_dir_x = ray->dir_x + ray->plane_x * ray->camera_x;
+	ray->ray_dir_y = ray->dir_y + ray->plane_y * ray->camera_x;
+	if (ray->ray_dir_x == 0)
+		ray->delta_dist_x = 1e30;
+	else
+		ray->delta_dist_x = fabs(1.f / ray->ray_dir_x);
+	if (ray->ray_dir_y == 0)
+		ray->delta_dist_y = 1e30;
+	else
+		ray->delta_dist_y = fabs(1.f / ray->ray_dir_y);
+}
+
+void	print_ray_param(t_ray *ray)
+{
+	printf("\x1B[H\x1B[Jdir_x : %lf\ndir_y : %lf\ndeltadir_x : %lf\n" \
+		"deltadir_y : %lf\n" \
+		"step_x : %lf\nstep_y : %lf\nside_dist_x : %lf\nsize_dist_y : %lf\n" \
+		"pos_x : %lf\npos_y : %lf\n" \
+		"plane_x : %lf\nplane_y : %lf\nray_dir_x : %lf\nray_dir_y : %lf\n" \
+		"hit : %d\nside : %d\nmap_pos:\n\t-x : %d\n\t-y : %d\ncamera_x : %lf\n" \
+		"perp_wall_dist : %lf\nend_ray.x : %d\nend_ray.y : %d\n",
+		ray->dir_x, ray->dir_y, ray->delta_dist_x, ray->delta_dist_y, \
+		ray->step_x, ray->step_y, ray->side_dist_x, ray->side_dist_y, \
+		ray->pos_x, ray->pos_y, ray->plane_x, ray->plane_y, ray->ray_dir_x, \
+		ray->ray_dir_y, ray->hit, ray->side, ray->map_pos.x, ray->map_pos.y, \
+		ray->camera_x, ray->perp_wall_dist, ray->end_ray.x, ray->end_ray.y);
+}
---
+++ src/keyboard.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   keyboard.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lrichaud <lrichaud@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/16 13:24:21 by lrichaud          #+#    #+#             */
+/*   Updated: 2024/11/18 12:02:23 by lrichaud         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cube3d.h"
+
+int	key_released(int keycode, t_mlx *vars)
+{
+	if (keycode == 'a')
+		vars->movement.left = 0;
+	else if (keycode == 'd')
+		vars->movement.right = 0;
+	else if (keycode == 'w')
+		vars->movement.forward = 0;
+	else if (keycode == 's')
+		vars->movement.down = 0;
+	return (0);
+}
+
+void	fps(t_mlx *vars)
+{
+	struct timeval timer;
+
+	gettimeofday(&timer, NULL);
+	printf("fps : %lu", vars->fps / (timer.tv_sec - vars->time.tv_sec));
+}
+
+int	key_hook( int keycode, t_mlx *vars)
+{
+	int	i;
+
+	i = 0;
+	if (keycode == 65307)
+	{
+		fps(vars);
+		mlx_do_key_autorepeaton(vars->mlx);
+		while (vars->map[i])
+			nfree(vars->map[i++]);
+		nfree(vars->map);
+		mlx_destroy_image(vars->mlx, vars->img.img);
+		mlx_destroy_window(vars->mlx, vars->win);
+		mlx_destroy_display(vars->mlx);
+		nfree(vars->mlx);
+		exit(1);
+	}
+	else if (keycode == 'd')
+		vars->movement.right = 1;
+	else if (keycode == 'a')
+		vars->movement.left = -1;
+	else if (keycode == 's')
+		vars->movement.down = 1;
+	else if (keycode == 'w')
+		vars->movement.forward = -1;
+	return (0);
+}
